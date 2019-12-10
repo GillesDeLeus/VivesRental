d692ec0f175691302930c7910a22c99db39b8eda
Bavo Ketels <bavo.ketels@vives.be>
2019-12-10 20:06:56 +0000

    Deleted VivesRental.RestApi.Contracts

--- VivesRental.RestApi.Contracts/Settings/SwaggerOptions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace VivesRental.RestApi.Contracts.Settings
-{
-    public class SwaggerOptions
-    {
-        public string JsonRoute { get; set; }
-        public string Description { get; set; }
-        public string UiEndpoint { get; set; }
-    }
-}
--- VivesRental.RestApi.Contracts/V1/Responses/AuthSuccessResponse.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace VivesRental.RestApi.Contracts.V1.Responses
-{
-    public class AuthSuccessResponse
-    {
-        public string Token { get; set; }
-        public string RefreshToken { get; set; }
-    }
-}

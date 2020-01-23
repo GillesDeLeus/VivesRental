51b037c37ec1620f90839d32cd182085bd8951f8
Bavo Ketels <bavo.ketels@vives.be>
2020-01-23 16:52:30 +0000

    Revert "Deleted VivesRental.Repository"

---
+++ VivesRental.Repository/Includes/ArticleIncludes.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace VivesRental.Repository.Includes
+{
+    public class ArticleIncludes
+    {
+        public bool Product { get; set; }
+    }
+}
---
+++ VivesRental.Repository/Includes/ArticleReservationIncludes.cs
@@ -0,0 +1,8 @@
+﻿namespace VivesRental.Repository.Includes
+{
+    public class ArticleReservationIncludes
+    {
+        public bool Article { get; set; }
+        public bool Customer { get; set; }
+    }
+}
